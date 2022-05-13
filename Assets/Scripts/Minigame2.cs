6bdc38c3b49501c8db139f610a5e86b8f73c2331
ghostentity12 <jamesleung.gd@gmail.com>
2022-05-13 11:33:38 +1000

    URP fix

--- Assets/Scripts/Minigame2.cs
+++ Assets/Scripts/Minigame2.cs
@@ -9,13 +9,20 @@ public class Minigame2 : MonoBehaviour
   [SerializeField] Image minigameImage;
   [SerializeField] bool active = false;
   bool completed = false;
-  [SerializeField] ProgressBar pb;
+  ProgressBar pb;
+  float progress;
   [SerializeField] float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pb = Instantiate(GameManager.Instance.progressBarPrefab, GameManager.Instance.c.transform);
+    }
 
     // Update is called once per frame
     void Update()
     {
       if (active) {
+        pb.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(transform.position + Vector3.up * 2);
 
         var rotateHorizontal = Input.GetAxis("Horizontal");
         var rotateVertical = Input.GetAxis("Vertical");
@@ -23,8 +30,9 @@ public class Minigame2 : MonoBehaviour
         Vector3 rotation = (Vector3.up * rotateHorizontal + Vector3.left * rotateVertical);
         if (rotateHorizontal != 0 || rotateVertical != 0) {
           minigameImage.transform.rotation = Quaternion.LookRotation(Vector3.forward, rotation);
+          progress += Time.deltaTime;
           pb.AddProgress(Time.deltaTime / duration);
-          if (pb.Complete) {
+          if (progress >= duration) {
             Debug.Log("finished");
             pb.gameObject.SetActive(false);
             active = false;
@@ -36,7 +44,7 @@ public class Minigame2 : MonoBehaviour
     }
 
     [ContextMenu("Enable")]
-    public void OnMinigameStart() {
+    void OnMinigameStart() {
       minigameCanvas.gameObject.SetActive(true);
       active = true;
     }
